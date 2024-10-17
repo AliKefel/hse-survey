f3021e22e53e3ccc8fa02e23bea55501d2123a35
Ali Kefel <alikefel114@gmail.com>
2024-10-17 00:28:22 -0700

    fixing container

--- server/index.js
+++ server/index.js
@@ -9,17 +9,18 @@ const app = express();
 const PORT = process.env.PORT || 5003;
 
 // Middleware
-const allowedOrigins = ['https://hse-survey.vercel.app'];
+// const allowedOrigins = ['https://hse-survey.vercel.app'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// }));
+app.use(cors());
 
 app.use(helmet()); // Add security headers
 app.use(express.json()); // To parse JSON body
